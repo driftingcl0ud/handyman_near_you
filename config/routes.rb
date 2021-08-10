bae0a7874efc5b0f444ef85a946a5b1227652ab5
gael lamothe <gaellamothe83@gmail.com>
2021-08-10 16:06:34 +0400

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   
-  #resources :missions, only: [:create]
+  resources :missions, only: [:index, :show, :new, :create] do
+  resources :handymen 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
