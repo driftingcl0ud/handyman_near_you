92705bce2721a1c013c6a5c41037b3af59ca2c49
gael lamothe <gaellamothe83@gmail.com>
2021-08-10 17:07:47 +0400

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   
-  resources :missions, only: [:index, :show, :new, :create] do
-  resources :handymen 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'dashboard', to: 'pages#dashboard', as: :dashboard #dashboard_path
+  resources :missions do
+    resources :quotations, only: [:create, :new]
   end
+  resources :handymen 
+  resources :quotations, only: [:destroy, :edit]
+  patch '/quotations/:id/approve', to: 'quotations#approve', as: :approve_quotation
+  patch '/quotations/:id/decline', to: 'quotations#decline', as: :decline_quotation
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
