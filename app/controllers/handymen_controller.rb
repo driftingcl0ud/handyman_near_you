282d22bbfbf40c9433c3289710217746a3307573
kaveesh sunassee <kaveesh.sunassee@gmail.com>
2021-08-10 14:19:05 +0400

    validation models handyman + quotation

--- app/controllers/handymen_controller.rb
+++ app/controllers/handymen_controller.rb
@@ -1,3 +1,47 @@
 class HandymenController < ApplicationController
-	
+	def index
+		@handymen =Handyman.all
+	end
+
+	def new
+		@handymen = Handyman.find(params[:handyman_id])
+
+	end
+
+	def show
+		@quotation = Quotation.find(params[:id])
+	end
+
+	def create
+		@
+
+	end
+
+
+
+
+
+
 end
+
+
+
+#   def show
+#     @reservation = Reservation.find(params[:id])
+#   end
+
+#   def create
+#     @tool = Tool.find(params[:tool_id])
+#     @reservation = Reservation.new(reservation_params)
+#     @reservation.tool = @tool
+#     @reservation.user = current_user
+#     @reservation.save
+#     redirect_to reservations_path
+#   end
+
+#   private
+
+#   def reservation_params
+#     params.require(:reservation).permit(:start_date, :return_date)
+#   end
+# end
--- app/models/handyman.rb
+++ app/models/handyman.rb
@@ -1,2 +1,7 @@
 class Handyman < ApplicationRecord
+	has_many :missions
+	has_many :quotation
+  belongs_to :user
+  validates :job_title, presence: :true
+  validates :average_price, presence: :true
 end
