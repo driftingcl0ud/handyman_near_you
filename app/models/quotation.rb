b14f25b139369371af6db106e186c2555e1503d8
gael lamothe <gaellamothe83@gmail.com>
2021-08-11 11:15:54 +0400

    container added

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -2,6 +2,6 @@ class Quotation < ApplicationRecord
   belongs_to :mission
   belongs_to :handyman
   validates :specifi_price, presence: :true
-  validates :handyman_description, presence: :true
-  validates :status, presence: :true, default: :false
+  validates :handyman_description, presence: true
+  validates :status, presence: :true, default: false
 end
