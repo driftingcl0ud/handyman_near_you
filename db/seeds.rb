19d96dd0fff28ab6b7e9b29f3dcc6cf904210773
priyanka kallee <priyankagurroby@gmail.com>
2021-08-11 14:42:34 +0100

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#create seed for user and handyman
+
+puts "start seed"
+
+users1 = User.create(first_name: 'Kaveesh', last_name: 'Sinopharm', email:'kaveesh@yahoo.com', password: "123456", address:"Royal road curepipe" )
+users2 = User.create(first_name: 'Priyanka', last_name: 'Kallee', email:'kallee@hotmail.com', password: "123456", address:"Flic en Flac" )
+users3 = User.create(first_name: 'Drifting', last_name: 'Cloud', email:'cloud@yahoo.com', password: "123456", address:"Beau Bassin" )
+
+
+
+puts "Finished"
