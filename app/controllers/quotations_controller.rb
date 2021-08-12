83722e217af3e08ecb209bc1170ab662d512405a
kaveesh sunassee <kaveesh.sunassee@gmail.com>
2021-08-12 10:23:32 +0400

    handyman controller+view

--- app/controllers/quotations_controller.rb
+++ app/controllers/quotations_controller.rb
@@ -1,2 +1,13 @@
 class QuotationsController < ApplicationController
+	
+  def create
+    @quotations = Quotation.find(params[:quotation_id])
+    @mission = Mission.new(mission_params)
+    @mission.quotation = @quotation
+    @mission.save
+    redirect_to mission_path
+  end
+
+
+
 end
