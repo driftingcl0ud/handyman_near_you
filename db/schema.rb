fbaafbee760b886203d038922cfba875a944b6c6
kaveesh sunassee <kaveesh.sunassee@gmail.com>
2021-08-12 16:06:40 +0400

    handyman-map upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_120023) do
+ActiveRecord::Schema.define(version: 2021_08_12_070442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2021_08_10_120023) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_handymen_on_user_id"
   end
 
