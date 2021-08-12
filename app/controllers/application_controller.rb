3b565fe4f661a869bcda76d7bc92fdccccbec584
priyanka kallee <priyankagurroby@gmail.com>
2021-08-12 12:15:34 +0100

    form authentication issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address])
 
-  protected
-    def configure_permitted_parameters
-      
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address])
-
-      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address])
-    end
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address])
+  end
 end
