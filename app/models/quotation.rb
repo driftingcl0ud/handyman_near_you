2bd91b063fed79e01c80b91f98b8f84784dd97f1
gael lamothe <gaellamothe83@gmail.com>
2021-08-16 16:39:01 +0400

    quotationscontroller

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -1,7 +1,7 @@
 class Quotation < ApplicationRecord
   belongs_to :mission
   belongs_to :handyman
-  validates :specifi_price, presence: :true
+  validates :specific_price, presence: :true
   validates :handyman_description, presence: true
   validates :status, presence: :true, default: false
 end
