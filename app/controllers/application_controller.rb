6f9972dc3d2e076c7cbb91048ba473d48477818a
kaveesh sunassee <kaveesh.sunassee@gmail.com>
2021-08-19 11:09:58 +0400

    title-tagline checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+  # app/controllers/application_controller.rb
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
   protected
     def configure_permitted_parameters
       
